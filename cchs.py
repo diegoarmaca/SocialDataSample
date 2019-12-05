58f6f1e4d0f88c53db3235f6c7cce0ec2aab15c1
Diego Mamanche Castellanos <46854417+diegoarmaca@users.noreply.github.com>
2019-12-05 11:24:28 -0500

    Add files via upload

---
+++ cchs.py
@@ -0,0 +1,186 @@
+import numpy as np
+from typing import Tuple, TextIO
+
+# Convenience Constant
+
+PROVINCE_CODES = (
+        ('10','NL'),
+        ('11','PE'),
+        ('12','NS'),
+        ('13','NB'),
+        ('24','QC'),
+        ('35','ON'),
+        ('46','MB'),
+        ('47','SK'),
+        ('48','AB'),
+        ('59','BC'),
+        ('60','NN') # NN is a combined code for NT, NU and YK
+    )
+
+# PART 1: PREPARATION
+
+# Change data types to:
+# - 'U2' for nominal
+# - int for ordinal
+# - float for interval
+# - float for ratio
+
+DATA_COLUMNS = [
+('alcofreq', int),
+('alcoweek', float),
+('biosex', 'U2'),
+('agegroup', int),
+('education', int),
+('fruitvegtot', float),
+('stressgen', int),
+('stresswork', int),
+('healthphys', int),
+('healthment', int),
+('satisfaction', int),
+('province', 'U2'),
+('hasdoctor', 'U2'),
+('bmi', float),
+('height', float),
+('weight', float),
+('incomegroup', int),
+('workhoursperweek', float),
+('firstlanguage', 'U2'),
+('yrsmokedaily', float)
+]
+
+
+# Fill out tuples with nominal, ordinal, interval, and ratio column names
+
+NOMINAL = ('biosex',
+           'province',
+           'hasdoctor',
+           'firstlanguage')
+ORDINAL = ('alcofreq',
+           'agegroup',
+           'education',
+           'stressgen',
+           'stresswork',
+           'healthphys',
+           'healthment',
+           'satisfaction',
+           'incomegroup')
+           
+INTERVAL = ()
+
+RATIO = ('alcoweek',
+         'fruitvegtot',
+         'bmi',
+         'height',
+         'weight',
+         'workhoursperweek',
+         'yrsmokedaily')
+
+
+
+
+# PART 2: FUNCTIONS
+
+def replace_nominal_codes(data: np.array, column_name: str, codes: Tuple[Tuple[str, str]]) -> None:
+    '''Precondition: column_name exists in data and is a nominal data measurement scale.
+    codes is a tuple of tuples containing (old code, new code).
+
+    Change all occurrences of old code to new code in column_name of data.
+    
+    >>> replace_nominal_codes(CCHS, 'biosex', (('1', 'M'),('2', 'F')))
+    >>> CCHS['biosex'][0]
+    'F'
+    >>> CCHS['biosex'][-1]
+    'M'
+    '''
+    
+     
+        
+        
+        
+def replace_missing_with_nan(data: np.array, column_name: str, missing_codes: Tuple[float]) -> None:
+    '''Precondition: column_name exists in data and is a ratio data measurement scale.
+    missing_codes is a tuple containing codes that denote missing data.
+    Convert all values in column column_name of data that match the codes 
+    in missing_codes to Not-A-Number values (np.nan)
+    
+    >>> replace_missing_with_nan(CCHS, 'alcoweek', (996,))
+    >>> CCHS['alcoweek'][2]
+    nan
+    >>> CCHS['alcoweek'][-3]
+    nan
+    '''
+    
+    pass
+        
+        
+        
+def write_categorical_csv(data: np.array, column_name: str, outfile: TextIO) -> None:
+    '''Precondition: column_name exists in data and is nominal or ordinal data measurement scale.
+    Write to outfile the label and counts of each category contained in column_name of data.
+    
+    >>> with open("csv/biosex.csv", 'w') as file:
+            write_categorical_csv(CCHS, 'biosex', file)
+    >>> with open("csv/biosex.csv", 'r') as check:
+            for line in check:
+                print(line.strip())
+    biosex,count
+    F,832
+    M,668
+    '''
+    
+    pass
+
+
+
+def write_column_summary_csv(data: np.array, column_name: str, outfile: TextIO) -> None:
+    '''Precondition: column_name exists in data and is a non-nominal data measurement scale.
+    Write to outfile a line containing comma-separated values as follows:
+    - The column name
+    - The median of values in the column
+    - (if interval or ratio) The mean of values in the column, ignoring nan
+    - (if interval or ratio) The standard deviation of values in the column, ignoring nan
+    
+    (Examples not required)
+    
+    '''
+    
+    pass
+
+
+if __name__ == "__main__":
+    
+    # Uncomment when Part 1 is complete
+    '''
+    CCHS = np.genfromtxt('CCHSX.csv', delimiter=',', skip_header=1, dtype=DATA_COLUMNS)
+    '''
+    
+    # Uncomment when replace_nominal_codes is complete
+    '''
+    replace_nominal_codes(CCHS, 'biosex', (('1','M'),('2','F')))
+    replace_nominal_codes(CCHS, 'province', PROVINCE_CODES)
+    replace_nominal_codes(CCHS, 'hasdoctor', (('1','Y'),('2','N')))
+    replace_nominal_codes(CCHS, 'firstlanguage', (('1','EN'),('2','FR'),('3','EF'),('4','NO')))
+    '''
+
+    # Uncomment when replace_missing_with_nan is complete
+    '''
+    replace_missing_with_nan(CCHS, 'alcoweek', (996,))
+    replace_missing_with_nan(CCHS, 'workhoursperweek', (996,))
+    '''
+    
+    # Uncomment when write_categorical_csv is complete
+    '''
+    for c in NOMINAL + ORDINAL:
+        with open("csv/"+c+'.csv', 'w') as f:
+            write_categorical_csv(CCHS, c, f)
+    '''
+    
+    # Uncomment when ordinal_column_csv is complete
+    '''
+    with open("csv/summary.csv", 'w') as f:
+        f.write("COLUMN,MEDIAN,MEAN,STDEV\n")
+        for n in ORDINAL + INTERVAL + RATIO:
+            ordinal_column_csv(CCHS, n, f)
+    '''
+    
+    pass
