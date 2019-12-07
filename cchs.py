01e0926bf76b2574317af40f929ff7b835a7c48b
kelichiu <keli.chiu.kc@gmail.com>
2019-12-07 07:45:10 -0500

    Modified include int(ordinal) data in summary file

--- cchs.py
+++ cchs.py
@@ -1,3 +1,4 @@
+import csv 
 import numpy as np
 from typing import Tuple, TextIO
 
@@ -92,7 +93,8 @@ def replace_nominal_codes(data: np.array, column_name: str, codes: Tuple[Tuple[s
     >>> CCHS['biosex'][-1]
     'M'
     '''
-    
+    for i in range(len(codes)):
+        data[column_name] = np.where(data[column_name] == codes[i][0], codes[i][1], data[column_name])
      
         
         
@@ -110,7 +112,8 @@ def replace_missing_with_nan(data: np.array, column_name: str, missing_codes: Tu
     nan
     '''
     
-    pass
+    for code in missing_codes:
+        data[column_name] = np.where(data[column_name] == code, 'nan', data[column_name])
         
         
         
@@ -128,7 +131,10 @@ def write_categorical_csv(data: np.array, column_name: str, outfile: TextIO) -> 
     M,668
     '''
     
-    pass
+    count_unique_val = np.unique(data[column_name], return_counts=True)
+    outfile.write(column_name + ',count\n')
+    for i in range(len(count_unique_val[0])):
+        outfile.write(str(count_unique_val[0][i]) + ',' + str(count_unique_val[1][i]) + '\n')
 
 
 
@@ -144,43 +150,39 @@ def write_column_summary_csv(data: np.array, column_name: str, outfile: TextIO) 
     
     '''
     
-    pass
+    median = str(round(np.nanmedian(data[column_name]), 2))
+    if str(data[column_name].dtype).startswith("int"):
+        outfile.write(column_name + "," + median +'\n')
+    if str(data[column_name].dtype).startswith("float"):
+        mean = str(round(np.nanmean(data[column_name]), 2))
+        stdev = str(round(np.nanstd(data[column_name]),2))
+        outfile.write(column_name + "," + median + ',' + mean + ',' + stdev +'\n')
 
 
 if __name__ == "__main__":
     
     # Uncomment when Part 1 is complete
-    '''
     CCHS = np.genfromtxt('CCHSX.csv', delimiter=',', skip_header=1, dtype=DATA_COLUMNS)
-    '''
     
     # Uncomment when replace_nominal_codes is complete
-    '''
     replace_nominal_codes(CCHS, 'biosex', (('1','M'),('2','F')))
     replace_nominal_codes(CCHS, 'province', PROVINCE_CODES)
     replace_nominal_codes(CCHS, 'hasdoctor', (('1','Y'),('2','N')))
     replace_nominal_codes(CCHS, 'firstlanguage', (('1','EN'),('2','FR'),('3','EF'),('4','NO')))
-    '''
 
     # Uncomment when replace_missing_with_nan is complete
-    '''
     replace_missing_with_nan(CCHS, 'alcoweek', (996,))
     replace_missing_with_nan(CCHS, 'workhoursperweek', (996,))
-    '''
     
     # Uncomment when write_categorical_csv is complete
-    '''
     for c in NOMINAL + ORDINAL:
         with open("csv/"+c+'.csv', 'w') as f:
             write_categorical_csv(CCHS, c, f)
-    '''
     
     # Uncomment when ordinal_column_csv is complete
-    '''
     with open("csv/summary.csv", 'w') as f:
         f.write("COLUMN,MEDIAN,MEAN,STDEV\n")
         for n in ORDINAL + INTERVAL + RATIO:
-            ordinal_column_csv(CCHS, n, f)
-    '''
+            write_column_summary_csv(CCHS, n, f)
     
     pass
